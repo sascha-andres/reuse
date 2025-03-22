b432a447075e24784c2b0bddf96b5b02c83ac92e
Sascha Andres <sascha.andres@outlook.com>
2025-03-22 11:19:18 +0100

    feat(te): add te package

---
+++ te/te.go
@@ -0,0 +1,45 @@
+package te
+
+import (
+	"fmt"
+	"testing"
+)
+
+// MustGet executes a function returning a value and error, failing the test immediately on error with a given message.
+func MustGet[T any](t *testing.T, msg string, f func() (T, error)) T {
+	r, err := f()
+	if err != nil {
+		t.Error(fmt.Sprintf("%s: ", msg), err)
+		t.FailNow()
+	}
+	return r
+}
+
+// ShouldGet executes a function returning a error, failing the test on error with a given message.
+func ShouldGet[T any](t *testing.T, msg string, f func() (T, error)) T {
+	r, err := f()
+	if err != nil {
+		t.Error(fmt.Sprintf("%s: ", msg), err)
+		t.Fail()
+	}
+	return r
+}
+
+// MustRun executes the provided function and reports a test failure with a message if the function returns an error
+// and fails immediately.
+func MustRun(t *testing.T, msg string, f func() error) {
+	err := f()
+	if err != nil {
+		t.Error(fmt.Sprintf("%s: ", msg), err)
+		t.FailNow()
+	}
+}
+
+// ShouldRun executes the provided function and reports a test failure with a message if the function returns an error.
+func ShouldRun(t *testing.T, msg string, f func() error) {
+	err := f()
+	if err != nil {
+		t.Error(fmt.Sprintf("%s: ", msg), err)
+		t.Fail()
+	}
+}
